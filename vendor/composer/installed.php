33e491554d3bf308f86e9ade88684a8914f9c030
yokoyama <S4g2wi-m69-xsmjk@outlook.com>
2021-04-25 15:04:11 +0900

    "Update composer"

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '5b03ef71993a840965bc95ece47af019c12f4b66',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '5b03ef71993a840965bc95ece47af019c12f4b66',
+    ),
+  ),
+);
