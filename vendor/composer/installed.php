27538834dd6c17a6aaa7ee00ad688145bcbfbeb6
yokoyama <S4g2wi-m69-xsmjk@outlook.com>
2021-04-26 11:20:23 +0900

    "Create composer"

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '3cc7c76a301e99939ed48e711fde30c3bb54f7a1',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '3cc7c76a301e99939ed48e711fde30c3bb54f7a1',
+    ),
+  ),
+);
