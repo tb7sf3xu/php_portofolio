e4b1e5174673385eac006370dccad72d7193331c
yokoyama <S4g2wi-m69-xsmjk@outlook.com>
2021-04-27 19:18:18 +0900

    "Change charset"

--- dbconnect.php
+++ dbconnect.php
@@ -3,7 +3,7 @@
    * userデータベースへの接続
    */
   function connectUser() {
-    $dsn = "mysql:dbname=heroku_0cf28b832cea3cb;host=us-cdbr-east-03.cleardb.com;port=3306;chrset=utf8";
+    $dsn = "mysql:dbname=heroku_0cf28b832cea3cb;host=us-cdbr-east-03.cleardb.com;port=3306;chrset=utf8mb4";
     $user = "b06f4f16610003";
     $password = "10144b02";
     
@@ -12,6 +12,7 @@
             PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
             PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
         ]);
+        $pdo->set_charset("utf8");
         return $pdo;
     } catch(PDOException $e) {
         echo "接続失敗です！".$e->getMessage();
@@ -25,7 +26,7 @@
    * goodsデータベースへの接続
    */
   function connectgoods() {
-    $dsn = "mysql:dbname=heroku_0cf28b832cea3cb;host=us-cdbr-east-03.cleardb.com;port=3306;chrset=utf8";
+    $dsn = "mysql:dbname=heroku_0cf28b832cea3cb;host=us-cdbr-east-03.cleardb.com;port=3306;chrset=utf8mb4";
     $user = "b06f4f16610003";
     $password = "10144b02";
     
@@ -33,6 +34,7 @@
         $dbinfo = new PDO($dsn, $user, $password, [
             PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
         ]);
+        $dbinfo->set_charset("utf8");
         return $dbinfo;
     } catch(PDOException $e) {
         echo "接続失敗です！".$e->getMessage();
